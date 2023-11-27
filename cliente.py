0642162d319deb062a3624cd0b97e7f7eae882ef
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2023-11-26 19:39:07 -0600

    Permite varios cliente pero atiende uno a la vez

---
+++ cliente.py
@@ -0,0 +1,86 @@
+import socket
+
+# Crea un socket TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Crear socket de cliente")
+
+# Conecta el socket a la dirección localhost y el puerto 8080
+print("Conectando al servidor")
+sock.connect(("localhost", 9999))
+
+# Tipos de mensajes: {codigo:x, mensaje:y}
+msg10 = {"codigo":10,"msg":"Solicitar al servidor un pokemon"}
+msg20 = {"codigo":20,"msg":"¿Capturar pokemon?","pokemon":'x-pokemon'}
+msg21 = {"codigo":21,"msg":"¿Intentar captura de nuevo?","intentos":0}
+msg23 = {"codigo":23,"msg":"Intentos de captura agotados"}
+msg30 = {"codigo":30,"msg":"Sí"}
+msg31 = {"codigo":31,"msg":"No"}
+msg32 = {"codigo":32,"msg":"Terminando sesión"}
+
+# Envia un mensaje al servidor
+print("Cliente, ¿Desea capturar un pokemon?")
+respuesta = input()
+if respuesta == "Sí":
+    print("Cliente: Enviando mensaje al servidor con codigo 10")
+    sock.send(str(msg10).encode())
+else:
+    print("Cliente: Enviando mensaje al servidor con codigo 32")
+    sock.send(str(msg32).encode())
+
+while True:
+    # Recibe una respuesta del servidor
+    data = sock.recv(1024)
+    if not data:
+        break
+
+    # Imprime la respuesta del servidor
+    respuesta_servidor = eval(data.decode())
+    codigo = respuesta_servidor['codigo']
+
+    if codigo == 20:
+        print("Cliente: Recibí codigo 20")
+        print("Cliente: Recibí", respuesta_servidor['msg']," con pokemon", respuesta_servidor['pokemon'])
+
+        print("Cliente, ¿Desea capturar el pokemon?"+respuesta_servidor['pokemon'])
+        respuesta = input()
+        if respuesta == "Sí":
+            print("Cliente: Enviando mensaje al servidor con codigo 30: Sí")
+            sock.send(str(msg30).encode())
+        else:
+            print("Cliente: Enviando mensaje al servidor con codigo 31: No")
+            sock.send(str(msg31).encode())
+        
+
+    if codigo == 21:
+        print("Cliente: Recibí codigo 21")
+        print("Cliente: Recibí", respuesta_servidor['msg']," con", respuesta_servidor['intentos'],"intentos")
+        print("Cliente: Enviando mensaje al servidor con codigo 30: Sí")
+        msg30={'codigo':30,'msg':'Sí'}
+        sock.send(str(msg30).encode())
+
+    if codigo == 22:
+        print("Cliente: Recibí codigo 22")
+        print("Cliente: Recibí", respuesta_servidor['msg']," con pokemon", respuesta_servidor['pokemon'])
+        print("Cliente, ¿Desea capturar otro pokemon?")
+        respuesta = input()
+        if respuesta == "Sí":
+            print("Cliente: Enviando mensaje al servidor con codigo 10: Solicitar al servidor un pokemon")
+            sock.send(str(msg30).encode())
+        else:
+            print("Cliente: Enviando mensaje al servidor con codigo 32: Terminando sesión")
+            sock.send(str(msg32).encode())
+
+
+
+    if codigo == 23:
+        print("Cliente: Recibí codigo 23")
+        print("Cliente: Recibí", respuesta_servidor['msg'])
+
+
+    if codigo == 32:
+        print("Cliente: Recibí codigo 32")
+        break
+
+
+# Cierra el socket
+sock.close()
---
+++ servidor.py
@@ -0,0 +1,85 @@
+import socket
+import random
+
+import threading
+from _thread import start_new_thread
+print_lock = threading.Lock()
+
+def threaded(conn, addr):
+    msg20 = {"codigo":20,"msg":"¿Capturar pokemon?","pokemon":'x-pokemon'}
+    msg21 = {"codigo":21,"msg":"¿Intentar captura de nuevo?","intentos":10}
+    msg22 = {"codigo":22,"msg":"Envia pokemon capturado","pokemon":'x-pokemon'}
+    msg23 = {"codigo":23,"msg":"Intentos de captura agotados"}
+    msg30 = {"codigo":30,"msg":"Sí"}
+    msg31 = {"codigo":31,"msg":"No"}
+    msg32 = {"codigo":32,"msg":"Terminando sesión"}
+    msg = {"codigo":0,"msg":"Mensaje de prueba"}
+    while True:
+        # Recibe un mensaje del cliente
+        data = conn.recv(1024)
+        if data:  
+            msg_cliente = eval(data.decode())
+            codigo = msg_cliente['codigo']
+
+            if codigo == 10:
+                print("Servidor: Recibí solicitud de pokemon")
+                msg20['pokemon'] = 'pokemon1'
+                #conn.sendall(str(msg20).encode())
+                print("Servidor: Enviando pokemon al cliente",addr[0],':',addr[1])
+                conn.send(str(msg20).encode())
+
+            if codigo == 30:
+                print("Servidor: Recibí respuesta afirmativa de captura de pokemon")
+                capturado = random.randint(0,1)
+
+                if capturado == 1:
+                    print("Servidor: Pokemon capturado")
+                    msg22 = {"codigo":22,"msg":"Pokemon capturado","pokemon":'x-pokemon'}
+                    msg22['pokemon'] = 'pokemon1'
+                    conn.send(str(msg22).encode())
+                else:
+                    print("Servidor: Pokemon no capturado")
+                    intentos = msg21['intentos'] - 1
+                    msg21 = {"codigo":21,"msg":"¿Intentar captura de nuevo?","intentos":intentos}
+                    conn.send(str(msg21).encode())
+                    intentos = intentos - 1
+                    if intentos == 0:
+                        print("Servidor: Intentos de captura agotados")
+                        conn.send(str(msg23).encode())
+
+            if codigo == 31:
+                print("Servidor: Recibí respuesta negativa de captura de pokemon")
+                print("Servidor: Enviando mensaje de terminar sesión")
+                conn.send(str(msg32).encode())
+
+            if codigo == 32:
+                print("Servidor: Recibí solicitud de terminar sesión")
+                conn.send(str(msg32).encode())
+                print_lock.release()
+                break
+        else:
+            print_lock.release()
+            break
+
+def Main():
+    host = ""
+    port = 9999
+    # Crea un socket TCP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Escucha conexiones en el puerto 9999
+    sock.bind((host, port))
+    print("El socket está enlazado al puerto", port)
+    sock.listen(5)
+    print("El socket está escuchando")
+	
+    while True:
+        # Acepta una conexión
+        conn, addr = sock.accept()
+        # Lock adquirido por el cliente
+        print_lock.acquire()
+        print('Connected to :', addr[0], ':', addr[1])
+        start_new_thread(threaded, (conn,addr))
+        #sock.close()
+
+if __name__ == '__main__':
+	Main()
